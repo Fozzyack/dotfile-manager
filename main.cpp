d496f0db0fcd679ebad5853b82f4b7b81eda9df1
Frasier <fsundra@gmail.com>
2025-04-05 01:34:13 +0800

    adds main menu

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <filesystem>
-using namespace std;
+#include "./src/headers/menu.h"
 
 
 int main() {
 
-    filesystem::path p("/home/fozzyack/code/dotfile_manager");
-    for(const auto& dir_entry : filesystem::recursive_directory_iterator(p)) {
-        cout << dir_entry.path() << endl;
-    }
-    cout << p.string() << endl;
-    cout << filesystem::exists(p) << endl;
+    system("clear");
 
+    int choice = -1;
+
+    // Main loop
+    while(true) {
+        print_options();
+        std::cin >> choice;
+        system("clear");
+
+    }
     return 0;
 }
