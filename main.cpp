4e9da50417e02b4489a2b0ca9f5f023ebce6e0f3
Frasier <fsundra@gmail.com>
2025-04-04 23:26:52 +0800

    testing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <filesystem>
 using namespace std;
 
 
 int main() {
-    cout << "Hello World" << endl;
+
+    filesystem::path p("/home/fozzyack/code/dotfile_manager");
+    for(const auto& dir_entry : filesystem::recursive_directory_iterator(p)) {
+        cout << dir_entry.path() << endl;
+    }
+    cout << p.string() << endl;
+    cout << filesystem::exists(p) << endl;
+
     return 0;
 }
